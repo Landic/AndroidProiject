c362f6e9474d1cf346917d2db89ca50b76518aa1
Danylo <danil.volkov.235@gmail.com>
2025-02-13 11:36:31 +0200

    update project

---
+++ app/src/main/java/com/example/androidproject/CalcActivity.java
@@ -0,0 +1,33 @@
+package com.example.androidproject;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalcActivity extends AppCompatActivity {
+    private TextView tvResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+        findViewById(R.id.calc_btn_0).setOnClickListener((this::onDigitClick));
+        findViewById(R.id.calc_btn_1).setOnClickListener((this::onDigitClick));
+        findViewById(R.id.calc_btn_2).setOnClickListener((this::onDigitClick));
+        findViewById(R.id.calc_btn_3).setOnClickListener((this::onDigitClick));
+        findViewById(R.id.calc_btn_4).setOnClickListener((this::onDigitClick));
+        findViewById(R.id.calc_btn_5).setOnClickListener((this::onDigitClick));
+        findViewById(R.id.calc_btn_6).setOnClickListener((this::onDigitClick));
+        findViewById(R.id.calc_btn_7).setOnClickListener((this::onDigitClick));
+        findViewById(R.id.calc_btn_8).setOnClickListener((this::onDigitClick));
+        findViewById(R.id.calc_btn_9).setOnClickListener((this::onDigitClick));
+        tvResult = findViewById(R.id.calc_tv_result);
+    }
+
+    private void onDigitClick(View view){
+        Button btn = (Button) view;
+        tvResult.setText(tvResult.getText().toString() + btn.getText());
+    }
+}
--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -59,9 +60,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onButton1Click(View view) {
-
-        String gText = tv1.getText().toString();
-        gText += "!!";
-        tv1.setText(gText);
+        startActivity(new Intent(MainActivity.this, CalcActivity.class));
     }
 }
